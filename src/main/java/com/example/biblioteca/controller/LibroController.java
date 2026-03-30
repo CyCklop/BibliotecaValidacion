72ea1f4282141b7da58527d87c22e77451c07a3b
CyCklop <ignacioandresflores06@gmail.com>
2026-03-30 12:01:19 -0300

    a0.5err

--- src/main/java/com/example/biblioteca/controller/LibroController.java
+++ src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -1,5 +1,82 @@
 package com.example.biblioteca.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.biblioteca.dto.ClientRequest;
+import com.example.biblioteca.exception.ResourceNotFoundException;
+import com.example.biblioteca.mapper.LibroMapper;
+import com.example.biblioteca.model.Libro;
+import com.example.biblioteca.service.LibroService;
+
+@RestController
+@RequestMapping("/api/v1/libros")
 public class LibroController {
-    
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> listarLibros() {
+        return libroService.getLibros();
+    }
+
+    //@PostMapping
+    //public Libro agregarLibro(@Valid @RequestBody ClientRequest request) {
+    //    return libroService.saveLibro(LibroMapper.toModel(request));
+    //}
+
+    @PostMapping
+    public ResponseEntity<?> agregarLibro(@Valid @RequestBody ClientRequest request, BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(error -> 
+                errores.put(error.getField(), error.getDefaultMessage())
+            );
+            return ResponseEntity.badRequest().body(errores);
+        }
+
+        return ResponseEntity.ok(libroService.saveLibro(LibroMapper.toModel(request)));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Libro> buscarLibro(@PathVariable int id){
+        Libro libro = libroService.getLibroId(id);
+        
+        if (libro == null) {
+            throw new ResourceNotFoundException("Libro no encontrado para id: " + id);
+        }
+        
+        return ResponseEntity.ok(libro);
+    }
+
+    @PutMapping("{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro){
+        // el id lo usaremos mas adelante
+        return libroService.updateLibro(libro);
+    }
+
+    @DeleteMapping("{id}")
+    public String eliminarLibro(@PathVariable int id) {
+        return libroService.deleteLibro(id);
+    }
+
+
+    @GetMapping("/total")
+    public int totalLibrosV2() {
+        return libroService.totalLibrosV2();
+    }
 }
--- src/main/java/com/example/biblioteca/mapper/LibroMapper.java
+++ src/main/java/com/example/biblioteca/mapper/LibroMapper.java
@@ -1,5 +1,17 @@
 package com.example.biblioteca.mapper;
 
-public class LibroMapper {
+import com.example.biblioteca.model.Libro;
+import com.example.biblioteca.dto.ClientRequest;
 
+public class LibroMapper {
+    public static Libro toModel(ClientRequest request) {
+        return new Libro(
+            request.getId(),
+            request.getIsbn(),
+            request.getTitulo(),
+            request.getEditorial(),
+            request.getFechaPublicacion(),
+            request.getAutor()
+        );
+    }
 }
